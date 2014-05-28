5dbe3027f15d6bac3b95cb9aa08ac723e87c55e5
pyos <pyos100500@gmail.com>
2014-05-28 19:23:42 +0400

    Add aiohttp to requirements.

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     url='https://github.com/pyos/dogeweb',
     packages=['dogeweb'],
     package_dir={'dogeweb': 'dogeweb'},
-    package_data={'dogeweb': ['*.dg']}
+    package_data={'dogeweb': ['*.dg']},
+    install_requires=['aiohttp'],
 )

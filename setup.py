ff22a5ccc6a925a0d50e5dde75ce4b08537eee88
pyos <pyos100500@gmail.com>
2014-05-28 16:52:22 +0400

    Add a setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name='dogeweb',
+    version='HEAD',
+    description='A functional web framework.',
+    author='pyos',
+    author_email='pyos100500@gmail.com',
+    url='https://github.com/pyos/dogeweb',
+    packages=['dogeweb'],
+    package_dir={'dogeweb': 'dogeweb'},
+    package_data={'dogeweb': ['*.dg']}
+)

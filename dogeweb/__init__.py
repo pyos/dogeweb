5ce4a473cff8e5afbfa162974e20d92e36f2826f
pyos <pyos100500@gmail.com>
2014-05-30 17:29:22 +0400

    Make abort, response, and static context-free.

--- dogeweb/__init__.py
+++ dogeweb/__init__.py
@@ -1,7 +1,5 @@
 import dg
 from . import main, route, request, response, websocket
-
-r       = route
-app     = main.Server
-abort   = response.ContextAware.abort
-jsonify = response.jsonify
+from . import route as r
+from .main import Server as app
+from .response import abort, redirect, static, jsonify

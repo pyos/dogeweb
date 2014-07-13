7766823a51af9af76d8492b76fde013def50941c
pyos <pyos100500@gmail.com>
2014-07-13 23:39:55 +0400

    Remove the `route` decorator, add `get` and `wraps` instead.
    Also, properties can only be used if `/dogeweb/property` is imported now.

--- dogeweb/__init__.py
+++ dogeweb/__init__.py
@@ -3,3 +3,16 @@ from . import main, route, request, response, websocket
 from . import route as r
 from .main import Server as app
 from .response import abort, redirect, static, jsonify
+
+
+def property(get, set_=None, del_=None, doc=None):
+    def _property_error(_):
+        raise AssertionError('cannot use this; `property` overriden by dogeweb')
+    if set_ or del_ or doc:
+        _property_error(None)
+    try:
+        get.setter  = _property_error
+        get.deleter = _property_error
+    except AttributeError:
+        pass
+    return get

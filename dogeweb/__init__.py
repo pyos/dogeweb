c71e866ebe5af752a543b2cb05ad558f90e2a4ec
pyos <pyos100500@gmail.com>
2015-05-19 22:43:19 +0300

    Refactor some stuff, part 1.

--- dogeweb/__init__.py
+++ dogeweb/__init__.py
@@ -1,18 +1,32 @@
 import dg
+
 from . import main, route, request, response, websocket
-from . import route as r
-from .main import Server as app
-from .response import abort, redirect, static, jsonify
+
+
+app      = main.Server
+abort    = response.abort
+redirect = response.redirect
+static   = response.static
+jsonify  = response.jsonify
+r        = route
 
 
 def property(get, set_=None, del_=None, doc=None):
+    '''A replacement for `property` that does not, in fact, create a `property`.
+
+        So `~> @x` becomes simply `self -> self.x`.
+
+    '''
     def _property_error(_):
-        raise AssertionError('cannot use this; `property` overriden by dogeweb')
+        assert False, 'cannot use this; `property` overriden by dogeweb'
+
     if set_ or del_ or doc:
         _property_error(None)
+
     try:
         get.setter  = _property_error
         get.deleter = _property_error
     except AttributeError:
         pass
+
     return get
